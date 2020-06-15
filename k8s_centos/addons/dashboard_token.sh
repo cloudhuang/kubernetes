ca48b848531909c075d8a39355d32ef3d668cd03
Liping Huang <liping.huang@live.com>
2020-06-15 09:11:10 +0800

    add k8s_centos

---
+++ k8s_centos/addons/dashboard_token.sh
@@ -0,0 +1,4 @@
+# Generate Dashboard token
+kubectl create serviceaccount dashboard-admin -n kube-system
+kubectl create clusterrolebinding dashboard-admin --clusterrole=cluster-admin --serviceaccount=kube-system:dashboard-admin
+kubectl describe secrets -n kube-system $(kubectl -n kube-system get secret | awk '/dashboard-admin/{print $1}')
