d9c2b93b7abe43864416aa34b93cbfcb9f0f6f60
Liping Huang <liping.huang2008@gmail.com>
2021-09-05 14:40:50 +0800

    add vagrant ansible kubernates

--- k8s-ansible/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-# Update hosts file
-echo "[TASK 1] Update /etc/hosts file"
-cat >> /etc/hosts<<EOF
-172.42.42.100 ansible.example.com ansible
-172.42.42.101 node1.example.com node1
-172.42.42.102 node2.example.com node2
-172.42.42.103 node3.example.com node3
-EOF
